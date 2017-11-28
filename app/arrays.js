6af8b1107ee3c1b4e274370887b6297880f284b4
Boyko Vatev <boyko.vatev@dbugg.me>
2017-11-28 14:06:45 +0200

    All assessment tests are PASS.

--- app/arrays.js
+++ app/arrays.js
@@ -1,141 +1,193 @@
 arraysAnswers = {
   /**
    * Find the 0 based index of item in arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to find in arr
    * @returns {Number} The index of item in arr, or -1 if item is not in arr.
    */
   indexOf: function indexOf(arr, item) {
-    // Implement a function, that returns the 0 based index of an element in an array.
+    return arr.indexOf(item);
   },
 
   /**
    * Determine the sum of the items of arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number} The numerical sum of all items in arr.
    */
   sum: function sum(arr) {
-
+    return arr.reduce(function(sum, element) {
+      return sum + element;
+    })
   },
 
   /**
-   * Create a new array with the same items as arr, excluding item 
-   * 
+   * Create a new array with the same items as arr, excluding item
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be excluded from the new array
    * @returns {Number[]} A new array containing all numbers from arr except item.
    */
   remove: function remove(arr, item) {
+    return arr.filter(function(element) {
+      return element !== item;
+    })
+  },
+  /**
+   * Return the same array, excluding item
+   *
+   * @param {Number[]} arr - An array of numbers
+   * @param {Number} item - A number to be excluded from the new array
+   * @returns {Number[]} The same array of numbers
+   */
+  removeWithoutCopy: function removeWithoutCopy(arr, item) {
+    for (i = 0; i < arr.length; i++) {
+      if (i == arr.indexOf(item)) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
 
   },
 
   /**
    * Adds a number, item, to the end of an array, arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be appended to the end of arr
    * @returns {Number[]} The array arr, with item appended.
    */
   append: function append(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   /**
    * Removes a number, item, from the end of an array, arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} The array arr, with the last element removed..
    */
   truncate: function truncate(arr) {
-
+    arr.splice(-1, 1);
+    return arr;
   },
 
   /**
    * Adds a number, item, to the beginning of an array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to append to the beginning of arr.
    * @returns {Number[]} The array arr, with the first element item added
    */
   prepend: function prepend(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
 
   /**
    * Removes the first element from an array
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} The array arr, with the first element item removed.
    */
   curtail: function curtail(arr) {
-
+    arr.splice(0, 1);
+    return arr;
   },
 
   /**
    * Combines the two arrays arr1 and arr2 together
-   * 
+   *
    * @param {Number[]} arr1 - An array of numbers
    * @param {Number[]} arr2 - An array of numbers
    * @returns {Number[]} A new array, with elements from arr1 and arr2 in that order.
    */
   concat: function concat(arr1, arr2) {
-
+    var arr = arr1.concat(arr2);
+    return arr;
   },
 
   /**
    * Insert a number item into an array arr at the 0 based position index.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to be inerted into arr
    * @param {Number} index - A 0 based index into the array arr.
    * @returns {Number[]} The array arr, with the number item inserted at position index.
    */
   insert: function insert(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   /**
    * Counts the number of times a number item appears in an array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} item - A number to count in arr
    * @returns {Number} The count of the number of times the number item appeared in arr.
    */
   count: function count(arr, item) {
-
+    var count = 0;
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] == item)
+        count++;
+    }
+    return count;
   },
 
   /**
    * Determines the number of duplicated numbers in the array arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} An array of numbers that appear in arr more than once.
    */
   duplicates: function duplicates(arr) {
+    var arrDuplicates = [];
 
+    arrSorted = arr.slice().sort();
+
+    for (i = 0; i < arrSorted.length; i++) {
+      if (arrSorted[i + 1] == arrSorted[i]) {
+        if (arrDuplicates[arrDuplicates.length - 1] != arrSorted[i]) {
+          arrDuplicates.push(arrSorted[i]);
+        }
+      }
+    }
+    return arrDuplicates;
   },
 
   /**
    * Squares each number in an array arr. Example: square([1, 2, 3]) returns [1, 4, 9].
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @returns {Number[]} A new array of numbers that contains the elements of arr squared.
    */
   square: function square(arr) {
-
+    var arrSquared = [];
+    arr.forEach(function(element) {
+      arrSquared.push(element * element);
+    })
+    return arrSquared;
   },
 
   /**
    * Finds the indices of the occurrences of a number target in an array of numbers arr.
-   * 
+   *
    * @param {Number[]} arr - An array of numbers
    * @param {Number} target - A number to find all occurences of.
    * @returns {Number[]} A new array of numbers which represent the indices of target in arr.
    */
   findAllOccurrences: function findAllOccurrences(arr, target) {
-
+    arrIndices = [];
+    arr.forEach(function(element, index) {
+      if (element == target)
+        arrIndices.push(index);
+    })
+    return arrIndices;
   },
 };
--- app/recursion.js
+++ app/recursion.js
@@ -9,27 +9,92 @@ recursionAnswers = {
       }
    *
    * Where ... is the same type of object
-   * 
+   *
    * @param {fileSystemObject} data - a file system object as described above
    * @param {String} dirName - a directory name the files are desired to be listed from.
    * Note: This parameter is optional. If it is not provided, list ALL files.
-   * 
+   *
    * @returns {Number[]} The files under the directory dirName, including subdiretories.
    */
   listFiles: function listFiles(data, dirName) {
+    var filesAll = [];
+    var dirNameFound = false;
 
+    function eachItem(dir) {
+      for (var k in dir) {
+
+        if (typeof dir[k] == "object") {
+
+          if (typeof dirName != "string") {
+            if (dir[k].length == 1 && typeof dir[k][0].dirName == "undefined") {
+
+              filesAll.push(dir[k][0]);
+            }
+
+            if (typeof dir[k].files != "undefined") {
+
+              filesAll = filesAll.concat(dir[k].files);
+            }
+            eachItem(dir[k]);
+          } else {
+            if (dirNameFound == false) {
+              if (typeof dir[k][0] != "undefined") {
+                if (dir[k][0].dirName == dirName) {
+
+                  dirNameFound = true;
+
+                  if (typeof dir[k][0].files != "undefined") {
+
+
+                    filesAll = filesAll.concat(dir[k][0].files);
+                  }
+
+                  eachItem(dir[k][0]);
+                }
+              }
+            } else {
+
+
+              if (typeof dir[k].files != "undefined") {
+
+
+                filesAll = filesAll.concat(dir[k].files);
+              }
+
+              eachItem(dir[k]);
+            }
+          }
+
+        }
+      }
+    }
+
+    eachItem(data);
+
+    return filesAll;
   },
 
   /**
    * Determines the fibonacci number at position n.
    * https://en.wikipedia.org/wiki/Fibonacci_number
-   * 
+   *
    * The first few fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
-   * 
+   *
    * @param {Number} n - the index of the fibonacci number desired
    * @returns {Number} The nth fibonacci number
    */
   fibonacci: function fibonacci(n) {
+    var a = 1,
+      f = 0,
+      temp;
 
+    while (n > 0) {
+      temp = a;
+      a = a + f;
+      f = temp;
+      n--;
+    }
+
+    return f;
   },
 };
